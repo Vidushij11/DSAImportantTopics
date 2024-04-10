f8b8da33d55284aa7e57453f1f61cd4207433a4c
vidushij11 <vidushij078@gmail.com>
2024-04-10 22:30:38 +0530

    Activity Selection update

--- GreedyAlgorithms/ActivitySelection.java
+++ GreedyAlgorithms/ActivitySelection.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
+import java.util.*;
 
 public class ActivitySelection {
     public static void main(String[] args) {
         int start[] = { 1, 3, 0, 5, 8, 5 };
         int end[] = { 2, 4, 6, 7, 9, 9 };
+
+        // to sort the end
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+        // lambda funtion -> short form
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
         int maxAct = 0;
         ArrayList<Integer> list = new ArrayList<>();
         maxAct = 1;
-        list.add(0);
-        int lastEnd = end[0];
+        list.add(activities[0][0]);
+        int lastEnd = activities[0][2];
         for (int i = 1; i < start.length; i++) {
-            if (start[i] >= lastEnd) {
+            if (activities[i][1] >= lastEnd) {
                 maxAct++;
-                list.add(i);
-                lastEnd = end[i];
+                list.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
 
